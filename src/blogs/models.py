e724a783ff70f60b6d7e9f29c0ad5549a3ae608a
davidjj76 <davidjj76@gmail.com>
2017-03-13 10:24:57 +0100

    Create models and migrations and register in admin site

--- src/blogs/models.py
+++ src/blogs/models.py
@@ -1,3 +1,31 @@
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+
+class Blog(models.Model):
+
+    name = models.CharField(max_length=100, unique=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    modified_at = models.DateTimeField(auto_now=True)
+    owner = models.OneToOneField(User, on_delete=models.CASCADE)
+
+
+class Category(models.Model):
+
+    name = models.CharField(max_length=100, unique=True)
+    abbreviation = models.CharField(max_length=3, unique=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    modified_at = models.DateTimeField(auto_now=True)
+
+
+class Post(models.Model):
+
+    title = models.CharField(max_length=100)
+    summary = models.CharField(max_length=255)
+    body = models.TextField()
+    media_url = models.URLField(null=True, blank=True)
+    published_at = models.DateTimeField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    modified_at = models.DateTimeField(auto_now=True)
+    blog = models.ForeignKey(Blog, on_delete=models.CASCADE)
+    categories = models.ManyToManyField(Category)
