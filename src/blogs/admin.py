aba534bd4c2575d8943409f6127c51cef6ec0d86
davidjj76 <davidjj76@gmail.com>
2017-03-13 13:02:28 +0100

    Customize admin site

--- src/blogs/admin.py
+++ src/blogs/admin.py
@@ -1,5 +1,48 @@
 from django.contrib import admin
+from django.utils import timezone
 
 from blogs.models import Blog, Category, Post
 
-admin.site.register([Blog, Category, Post])
+
+@admin.register(Blog)
+class BlogAdmin(admin.ModelAdmin):
+
+    list_display = ('name', 'owner')
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+
+    list_display = ('name', 'abbreviation')
+
+
+class StatusListFilter(admin.SimpleListFilter):
+
+    title = 'Status'
+    parameter_name = 'status'
+
+    def lookups(self, request, model_admin):
+        return (
+            ('DR', 'Draft'),
+            ('PB', 'Published')
+        )
+    def queryset(self, request, queryset):
+
+        if self.value() == 'DR':
+            return queryset.filter(published_at__gt=timezone.now())
+
+        if self.value() == 'PB':
+            return queryset.filter(published_at__lte=timezone.now())
+
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+
+    list_display = ('title', 'blog', 'get_owner', 'published_at', 'status', 'tagged_with')
+    list_filter = ('blog', 'blog__owner', 'categories', StatusListFilter)
+
+    def get_owner(self, obj):
+        return obj.blog.owner
+
+    get_owner.short_description = 'Owner'
+    get_owner.admin_order_field = 'blog__owner'
--- src/blogs/models.py
+++ src/blogs/models.py
@@ -1,5 +1,6 @@
 from django.contrib.auth.models import User
 from django.db import models
+from django.utils import timezone
 
 
 class Blog(models.Model):
@@ -9,6 +10,12 @@ class Blog(models.Model):
     modified_at = models.DateTimeField(auto_now=True)
     owner = models.OneToOneField(User, on_delete=models.CASCADE)
 
+    class Meta:
+        ordering = ["owner"]
+
+    def __str__(self):
+        return self.name
+
 
 class Category(models.Model):
 
@@ -16,6 +23,13 @@ class Category(models.Model):
     abbreviation = models.CharField(max_length=3, unique=True)
     created_at = models.DateTimeField(auto_now_add=True)
     modified_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ["abbreviation"]
+        verbose_name_plural = "Categories"
+
+    def __str__(self):
+        return self.name
 
 
 class Post(models.Model):
@@ -29,3 +43,15 @@ class Post(models.Model):
     modified_at = models.DateTimeField(auto_now=True)
     blog = models.ForeignKey(Blog, on_delete=models.CASCADE)
     categories = models.ManyToManyField(Category)
+
+    class Meta:
+        ordering = ["-published_at"]
+
+    def __str__(self):
+        return self.title
+
+    def status(self):
+        return "Published" if self.published_at <= timezone.now() else "Draft"
+
+    def tagged_with(self):
+        return ", ".join([c.name for c in self.categories.all()])
