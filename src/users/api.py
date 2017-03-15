2d8eec91d5aab7195c3c2ea58e5c86cf059f87dd
davidjj76 <davidjj76@gmail.com>
2017-03-15 22:41:10 +0100

    Added endpoint for create users

--- src/users/api.py
+++ src/users/api.py
@@ -1,6 +1,7 @@
 from django.contrib.auth.models import User
 from rest_framework.response import Response
 from rest_framework.views import APIView
+from rest_framework import status
 
 from users.serializers import UserSerializer
 
@@ -8,6 +9,24 @@ from users.serializers import UserSerializer
 class UsersAPI(APIView):
 
     def get(self, request):
+        """
+        Return a list of system users
+        :param request: HttpRequest object
+        :return: HttpResponse object
+        """
         users = User.objects.all()
         serializer = UserSerializer(users, many=True)
         return Response(serializer.data)
+
+    def post(self, request):
+        """
+        Creates a user
+        :param request: HttpRequest object
+        :return: HttpResponse object
+        """
+        serializer = UserSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        else:
+            return  Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
--- src/users/serializers.py
+++ src/users/serializers.py
@@ -1,16 +1,25 @@
+from django.contrib.auth.models import User
 from rest_framework import serializers
 
 
 class UserSerializer(serializers.Serializer):
 
-    id = serializers.CharField()
+    id = serializers.ReadOnlyField()
     username = serializers.CharField()
     email = serializers.EmailField()
     first_name = serializers.CharField()
     last_name = serializers.CharField()
+    password = serializers.CharField()
 
     def create(self, validated_data):
-        pass
+        user = User()
+        user.username = validated_data.get('username')
+        user.email = validated_data.get('email')
+        user.first_name = validated_data.get('first_name')
+        user.last_name = validated_data.get('last_name')
+        user.set_password(validated_data.get('password'))
+        user.save()
+        return user
 
     def update(self, instance, validated_data):
         pass
