25f38af0e9498bd988b65c9c3699f376874676d1
davidjj76 <davidjj76@gmail.com>
2017-03-19 01:15:57 +0100

    Tune admin site

--- src/blogs/admin.py
+++ src/blogs/admin.py
@@ -32,6 +32,37 @@ class StatusListFilter(admin.SimpleListFilter):
 @admin.register(Post)
 class PostAdmin(admin.ModelAdmin):
 
-    list_display = ('title', 'author', 'published_at', 'status', 'tagged_with')
+    list_display = ('title', 'get_author', 'media_url', 'summary', 'published_at', 'status')
     list_filter = ('author', 'categories', StatusListFilter)
     search_fields = ('title', 'summary', 'body')
+
+    fieldsets = (
+        ('Author and Title', {
+            'fields': ('author', 'title'),
+            }
+        ),
+        ('Main content', {
+            'fields': ('summary', 'body'),
+            }
+        ),
+        ('Multimedia content', {
+            'fields': ('media_url',),
+            'classes': ('collapse', 'extrapretty')
+            }
+        ),
+        ('Published', {
+            'fields': ('published_at',),
+            'description': 'Set publication date of post (you can save as draft publishing at future date)'
+            }
+        ),
+        ('Tagged with', {
+            'fields': ('categories',),
+            'description': 'At least one category'
+            }
+        )
+    )
+
+    def get_author(self, obj):
+        return obj.author.username
+    get_author.short_description = 'author'
+    get_author.admin_order_field = 'author__username'
